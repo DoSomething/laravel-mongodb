02c50e443eac1262ec933dc7abc253d3f065d12a
David Furnes <david@dfurnes.com>
2017-11-02 10:19:58 -0400

    Fix issues with DatabaseTokenRepository.
    For more details: https://git.io/vFGsV

--- src/Jenssegers/Mongodb/Auth/DatabaseTokenRepository.php
+++ src/Jenssegers/Mongodb/Auth/DatabaseTokenRepository.php
@@ -14,7 +14,9 @@ class DatabaseTokenRepository extends BaseDatabaseTokenRepository
      */
     protected function getPayload($email, $token)
     {
-        return ['email' => $email, 'token' => $token, 'created_at' => new UTCDateTime(time() * 1000)];
+        $hashedToken = $this->hasher->make($token);
+
+        return ['email' => $email, 'token' => $hashedToken, 'created_at' => new UTCDateTime(time() * 1000)];
     }
 
     /**
@@ -22,16 +24,11 @@ class DatabaseTokenRepository extends BaseDatabaseTokenRepository
      */
     protected function tokenExpired($token)
     {
-        // Convert UTCDateTime to a date string.
-        if ($token['created_at'] instanceof UTCDateTime) {
-            $date = $token['created_at']->toDateTime();
-            $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $token['created_at'] = $date->format('Y-m-d H:i:s');
-        } elseif (is_array($token['created_at']) && isset($token['created_at']['date'])) {
-            $date = new DateTime($token['created_at']['date'], new DateTimeZone(isset($token['created_at']['timezone']) ? $token['created_at']['timezone'] : 'UTC'));
-            $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $token['created_at'] = $date->format('Y-m-d H:i:s');
-        }
+        /** @var UTCDateTime $token */
+
+        $date = $token->toDateTime();
+        $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
+        $token = $date->format('Y-m-d H:i:s');
 
         return parent::tokenExpired($token);
     }
